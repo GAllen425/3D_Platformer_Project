f7908193ca71493590904587e7da436ad4909b8a
DarylYoung94 <58364752+DarylYoung94@users.noreply.github.com>
2019-12-02 23:57:14 +0000

    Squirrel/Frog

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    
+    private Animator animator;
+    //private bool isAnimating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalkingPressed = (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.A)) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D));
+        animator.SetBool("isRunning", isWalkingPressed);
+
+       
+    }
+}
