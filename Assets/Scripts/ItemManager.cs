11bb9997b75754ae39f16e4238f1b6acaf803fd9
gallen <gallen.coding@gmail.com>
2019-12-03 22:10:58 +0000

    Scoring

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -7,8 +7,7 @@ public class ItemManager : MonoBehaviour
 {
     public Dictionary<string,int> itemDictionary;
     public UnityEvent invokeMethod;
-    public int countAcorn = 0;
-    // Start is called before the first frame update
+
     void Start()
     {
         itemDictionary = new Dictionary<string, int>();
@@ -16,7 +15,6 @@ public class ItemManager : MonoBehaviour
         invokeMethod.Invoke();
     }
 
-    // Update is called once per frame
     void Update()
     {
     }
@@ -32,11 +30,6 @@ public class ItemManager : MonoBehaviour
     {    
         int currentCount = 0;
         itemDictionary.TryGetValue(name, out currentCount); 
-        itemDictionary[name] = currentCount - 1;        
-    }
-
-    public void Count(string name)
-    {
-        countAcorn = itemDictionary[name];
+        itemDictionary[name] = currentCount - 1; 
     }
 }
