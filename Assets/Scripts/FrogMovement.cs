18d2184febae31172a912c4683ef83743f8efb9d
DarylYoung94 <58364752+DarylYoung94@users.noreply.github.com>
2019-12-14 11:05:40 +0000

    frog

---
+++ Assets/Scripts/FrogMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMovement : MonoBehaviour
+{
+    private Animator animator;
+    private bool isWandering = false;
+    private bool isRotatingRight = false;
+    private bool isRotatingLeft = false;
+    private bool isWalking = false;
+    public float rotSpeed = 100f;
+    public float moveSpeed = 1f;
+    private bool isMoving = false;
+    public Rigidbody frog;
+    public float jumpForce;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        frog = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isMoving", false);
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if(isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+        if (isWalking == true)
+        {
+            animator.SetBool("isMoving",true);            
+
+            frog.AddRelativeForce(Vector3.left * jumpForce, ForceMode.Impulse);
+            frog.AddRelativeForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 4);
+        int rotateLorR = Random.Range(0, 3);
+        int walkWait = Random.Range(1, 4);
+        int walkTime = Random.Range(1, 2);
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateLorR ==1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR ==2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+}
